2dd03d2850dea025b578d98b4881ea6a7a80094e
emmaMarful <eamarful@gmail.com>
2023-06-12 16:55:50 +0000

    added the title, explanation and picture of the astronomy picture of the day

---
+++ Home.py
@@ -0,0 +1,42 @@
+import streamlit as web_ui
+import requests
+
+
+header = "Astronomy Picture Of the Day"
+web_ui.title(header.title())
+
+# nasa api key
+apiKey = "ma4c1m3W5TFYAIyypmgkDbAwLLrb8aHbwWyOgNsQ"
+
+# url to astronomy picture of the day
+url ="https://api.nasa.gov/planetary/apod?" \
+     f"api_key={apiKey}"
+
+# request data from api
+response = requests.get(url=url)
+
+# api data stored
+strApiData = response.json()
+
+# display title of astronomy picture of the day on webpage
+web_ui.header(strApiData["title"])
+
+
+# extract apod image
+img_url = strApiData["url"]
+
+# create a jpg to say apod
+crt_img = "image.jpg"
+extract_img = requests.get(img_url)
+
+with open(crt_img, "wb") as imageSave:
+    imageSave.write(extract_img.content)
+
+# display extracted web image to the web
+web_ui.image("image.jpg")
+date = f"Date: {strApiData['date']}"
+web_ui.write(date)
+
+# display explation of picture of the day
+desc =strApiData["explanation"]
+web_ui.write(desc)
